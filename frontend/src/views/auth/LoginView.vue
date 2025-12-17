ce0f8dcd4b649fc22f2bb06f1313cae61e496b5a
Eduardo Elizondo <eduardo.elizondof@gmail.com>
2025-12-17 15:35:59 -0600

    agendar

--- frontend/src/views/auth/LoginView.vue
+++ frontend/src/views/auth/LoginView.vue
@@ -113,7 +113,7 @@
       <!-- Link para clientes -->
       <div class="client-link">
         <p>¿Eres cliente?</p>
-        <router-link to="/" class="client-link-btn">
+        <router-link to="/agendar" class="client-link-btn">
           Agendar cita
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <polyline points="9 18 15 12 9 6"></polyline>
